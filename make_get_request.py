40e25bde53647b3ee8f267ebb7768c8d365de7e8
Dylan Uecker <dylan.uecker@gmail.com>
2022-08-27 12:19:52 -0400

    Less time and headache

--- make_get_request.py
+++ make_get_request.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
 
-import requests
 import hashlib
 import secrets
 import string
 import sys
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-driver = webdriver.Firefox()
 
 with open("client_credentials.txt") as file:
     client_id = file.readline()
     client_secret = file.readline()
 
-with open("user_credentials.txt") as file:
-    username = file.readline()
-    password = file.readline()
-
 state = "".join(secrets.choice(string.ascii_letters + string.digits) for i in range(20))
 scope = "" # need to add these
 code_verifier = "".join(secrets.choice(string.ascii_letters + string.digits) for i in range(secrets.randbelow(128 - 43 + 1) + 43))
 hashed_code_verifier = hashlib.sha256(code_verifier.encode("utf-8")).hexdigest()
+
+with open("get_request_params.txt", "w") as file:
+    file.write(state + "\n")
+    file.write(code_verifier + "\n")
 
 payload = {
     "client_id" : client_id,
@@ -35,19 +28,9 @@ payload = {
     "code_challenge" : hashed_code_verifier
 }
 
-response = requests.get("https://accounts.spotify.com/authorize", params = payload)
-print(response.url)
-driver.get(response.url)
-driver.find_element(By.ID, "login-username").send_keys(username)
-driver.find_element(By.ID, "login-password").send_keys(password)
-driver.find_element(By.ID, "login-button").click()
+url = "https://accounts.spotify.com/authorize?"
+for key, value in payload.items():
+    url += key + "=" + value + "&"
 
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[text()='Agree']"))).click()
-
-# open response.url with Selenium here to click the button
-#if response[1] != state:
-#    sys.exit("Mismatched state parameter")
-
-# need to proceed here, but will try logging in first
-driver.quit()
+print(url)
 
