c768e4abb86bd7e3c73217344bcff3cc9741e326
Dylan Uecker <dylan.uecker@gmail.com>
2022-08-27 11:14:14 -0400

    Automating by using selenium

--- export.py
+++ export.py
@@ -5,12 +5,20 @@ import hashlib
 import secrets
 import string
 import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# use Selenium to then click the button 
+driver = webdriver.Firefox()
 
 with open("client_credentials.txt") as file:
     client_id = file.readline()
     client_secret = file.readline()
+
+with open("user_credentials.txt") as file:
+    username = file.readline()
+    password = file.readline()
 
 state = "".join(secrets.choice(string.ascii_letters + string.digits) for i in range(20))
 scope = "" # need to add these
@@ -28,9 +36,18 @@ payload = {
 }
 
 response = requests.get("https://accounts.spotify.com/authorize", params = payload)
+print(response.url)
+driver.get(response.url)
+driver.find_element(By.ID, "login-username").send_keys(username)
+driver.find_element(By.ID, "login-password").send_keys(password)
+driver.find_element(By.ID, "login-button").click()
+
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[text()='Agree']"))).click()
+
 # open response.url with Selenium here to click the button
 #if response[1] != state:
 #    sys.exit("Mismatched state parameter")
 
 # need to proceed here, but will try logging in first
+driver.quit()
 
