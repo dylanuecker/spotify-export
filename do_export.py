b1cf0225dffbb9fcba81cd7f523e445c5f1628eb
Dylan Uecker <dylan.uecker@gmail.com>
2022-08-28 15:23:41 -0400

    It's alive!

--- do_export.py
+++ do_export.py
@@ -12,6 +12,17 @@ headers = {
     "Content-Type" : "application/json"
 }
 
-saved_tracks = requests.get(base_url + "/me/tracks", headers = headers)
-print(saved_tracks.json())
+# saved tracks first (saved local files are not counted, see Spotify Local Music for these if so desired)
+total = requests.get(base_url + "/me/tracks", headers = headers).json()["total"]
+
+with open("saved_tracks.txt", "w") as file:
+    for i in range(0, total, 50):
+        payload = {
+            "limit" : 50,
+            "offset" : i
+        }
+
+        for saved_track in requests.get(base_url + "/me/tracks", headers = headers, params = payload).json()["items"]:
+            print(saved_track)
+            #file.write(saved_track + "\n")
 
